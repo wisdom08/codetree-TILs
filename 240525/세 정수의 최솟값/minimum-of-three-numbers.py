6ea6bbfad945a6ca5f8fa41b255810dd100c6de9
wisdom08 <wisdom08@kakao.com>
2024-05-25 00:11:31 +0900

    Title: 세 정수의 최솟값, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240525/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240525/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,8 +1,8 @@
 arr = list(map(int, input().split()))
 
-if arr[0] < arr[1] and arr[0] < arr[2]:
+if arr[0] <= arr[1] and arr[0] <= arr[2]:
     print(arr[0])
-elif arr[1] < arr[0] and arr[1] < arr[2]:
+elif arr[1] <= arr[0] and arr[1] <= arr[2]:
     print(arr[1])
 else:
     print(arr[2])
