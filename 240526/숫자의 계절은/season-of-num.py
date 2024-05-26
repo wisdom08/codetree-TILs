4c0d7a2880d69be7d608374e1ee96f90f6cdd8cb
wisdom08 <wisdom08@kakao.com>
2024-05-26 09:47:23 +0900

    Title: 숫자의 계절은, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240526/숫자의 계절은/season-of-num.py
+++ 240526/숫자의 계절은/season-of-num.py
@@ -4,7 +4,7 @@ if m >= 3 and m <= 5:
     print("Spring")
 elif m >= 6 and m <= 8:
     print("Summer")
-elif m >= 6 and m <= 8:
+elif m >= 9 and m <= 11:
     print("Fall")
 else:
     print("Winter")
