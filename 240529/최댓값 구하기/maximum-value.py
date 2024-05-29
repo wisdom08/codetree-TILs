81434ce035605834fc0e72308555251238e1b81f
wisdom08 <wisdom08@kakao.com>
2024-05-29 23:48:31 +0900

    Title: 최댓값 구하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240529/최댓값 구하기/maximum-value.py
+++ 240529/최댓값 구하기/maximum-value.py
@@ -1,2 +1,8 @@
 arr = list(map(int, input().split()))
-print(max(arr))
+
+if arr[0] >= arr[1] and arr[0] >= arr[2]:
+    print(arr[0])
+elif arr[1] >= arr[0] and arr[1] >= arr[2]:
+    print(arr[1])
+else:
+    print(arr[2])
