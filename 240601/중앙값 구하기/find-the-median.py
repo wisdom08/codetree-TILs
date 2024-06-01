44b29bd1406c53deb9c47db42ca09c0ba5ba7fb5
wisdom08 <wisdom08@kakao.com>
2024-06-01 09:59:05 +0900

    Title: 중앙값 구하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240601/중앙값 구하기/find-the-median.py
+++ 240601/중앙값 구하기/find-the-median.py
@@ -1,2 +1,22 @@
 arr = list(map(int, input().split()))
-print(sorted(arr)[1])
+a = arr[0]
+b = arr[1]
+c = arr[2]
+
+if a > b and a > c:
+    if b > c:
+        print(b)
+    else:
+        print(c)
+
+elif b > a and b > c:
+    if a > c:
+        print(a)
+    else:
+        print(c)
+
+elif c > a and c > b:
+     if b > a:
+        print(b)
+     else:
+        print(a)
