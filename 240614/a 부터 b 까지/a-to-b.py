c6fb4569d835461f356fd3c10bfa5bd97216ba29
wisdom08 <wisdom08@kakao.com>
2024-06-14 11:59:08 +0900

    Title: a 부터 b 까지, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240614/a 부터 b 까지/a-to-b.py
+++ 240614/a 부터 b 까지/a-to-b.py
@@ -1,14 +1,8 @@
 a, b = map(int, input().split())
 
-print(a, end=" ")
-while True:
+while a <= b:
+    print(a, end=" ")
     if a % 2 == 1:
-        a *= 2
-        if a > b:
-            break
-        print(a, end=" ")
-    elif a % 2 == 0:
-        a +=3
-        if a > b:
-            break 
-        print(a, end=" ")
+        a*=2
+    else:
+        a+=3
