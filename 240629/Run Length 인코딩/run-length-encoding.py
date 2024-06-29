31260b5e49e2210f9e44a34a94c5a3af53202404
wisdom08 <wisdom08@kakao.com>
2024-06-29 11:30:57 +0900

    Title: Run Length 인코딩, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240629/Run Length 인코딩/run-length-encoding.py
+++ 240629/Run Length 인코딩/run-length-encoding.py
@@ -5,13 +5,14 @@ count = 1
 for i in range(0, len(s)-1):
     if s[i] == s[i+1]:
         count += 1
-        if i == len(s)-2:
-            res.append(s[i])
-            res.append(str(count))
     else:
         res.append(s[i])
         res.append(str(count))
         count = 1
+    
+    if i == len(s)-2:
+        res.append(s[i])
+        res.append(str(count))
     
 
 print(len(res))
