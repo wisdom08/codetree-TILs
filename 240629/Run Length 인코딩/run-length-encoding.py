0c9a4262181df58d6f8955bade16d56ee4db862a
wisdom08 <wisdom08@kakao.com>
2024-06-29 11:33:04 +0900

    Title: Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240629/Run Length 인코딩/run-length-encoding.py
+++ 240629/Run Length 인코딩/run-length-encoding.py
@@ -2,18 +2,22 @@ s = input()
 
 res = []
 count = 1
-for i in range(0, len(s)-1):
-    if s[i] == s[i+1]:
-        count += 1
-    else:
-        res.append(s[i])
-        res.append(str(count))
-        count = 1
-    
-    if i == len(s)-2:
-        res.append(s[i+1])
-        res.append(str(count))
+
+if len(s) == 1:
+    print(s+1)
+else:
+    for i in range(0, len(s)-1):
+        if s[i] == s[i+1]:
+            count += 1
+        else:
+            res.append(s[i])
+            res.append(str(count))
+            count = 1
+        
+        if i == len(s)-2:
+            res.append(s[i+1])
+            res.append(str(count))
     
 
-print(len(res))
-print(''.join(res))
+    print(len(res))
+    print(''.join(res))
